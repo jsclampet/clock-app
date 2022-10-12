667da2290e7927a8380226e8e2f1e351d82dcb7a
John Clampet <jsclampet@gmail.com>
2022-10-12 08:36:07 -0600

    added styling and data for expanded section. still needs button event listener for pop up

--- app.js
+++ app.js
@@ -24,56 +24,62 @@ refreshBtn.addEventListener('click', () => {
 })
 
 
-var timeApi = new XMLHttpRequest();
-timeApi.onreadystatechange = function() {
+var locationApi = new XMLHttpRequest();
+locationApi.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       let data = JSON.parse(timeApi.responseText)
-       console.log('weekday?', data)
-
-       let time = document.querySelector('#time');
-       time.textContent = data.timezone.current_time.slice(0,5)
-
-       let timeZone = document.querySelector('#time-zone-abbrev');
-       timeZone.textContent = data.timezone.abbreviation;
-
-       let location = document.querySelector('#location');
-       location.textContent = `${data.city}, ${data.country}`;
-
-       let currentTimezone = document.querySelector('.time-zone-full');
-        //    currentTimezone.textContent = data.
-
-
-        //geolocator (IP) || City, Country
-        //https://freegeoip.app/
-        var locationApi = new XMLHttpRequest();
-        locationApi.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            let locationData = JSON.parse(locationApi.responseText);
-                console.log('geo', locationData)
-
-            }
-        };
-        locationApi.open("GET", "https://api.ipbase.com/v2/info?format=json");
-        locationApi.setRequestHeader("apikey", "y9FpuihLeu2pDN80wl9Nfx7vVk8UbfGWklGOMVV5");
-        locationApi.send();
-
-
-        //NESTED API
-        // http://worldtimeapi.org/
-        // URL format ... http://worldtimeapi.org/api/timezone/america/denver  (<<country, city)
-        var worldTimeApi = new XMLHttpRequest();
-        worldTimeApi.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            let worldTimeApiData = JSON.parse(worldTimeApi.responseText)
-            console.log(worldTimeApiData)
-            }
-        };
-        worldTimeApi.open("GET", `http://worldtimeapi.org/api/timezone/${data.country}/${data.city}`, true);
-        worldTimeApi.send();
+    // Typical action to be performed when the document is ready:
+    let locationData = JSON.parse(locationApi.responseText);
+        console.log('geo', locationData)
+            
+        let city = locationData.data.location.city.name
+        let country = locationData.data.location.country.alpha2 
+        document.querySelector('#location').textContent = `IN ${locationData.data.timezone.id.split('/')[1].toUpperCase()}, ${locationData.data.location.country.alpha2.toUpperCase()}`
     }
 };
-timeApi.open("GET", "https://ipgeolocation.abstractapi.com/v1/?api_key=c9e14f3cb3e84944a0c1db81e9cb1f46&ip_address", true);
-timeApi.send();
+locationApi.open("GET", "https://api.ipbase.com/v2/info?format=json");
+locationApi.setRequestHeader("apikey", "y9FpuihLeu2pDN80wl9Nfx7vVk8UbfGWklGOMVV5");
+locationApi.send();
+
+
+var worldApi = new XMLHttpRequest();
+        worldApi.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+            let worldApiData = JSON.parse(worldApi.responseText)
+            console.log('worldApi', worldApiData)
+            
+            let time = document.getElementById('time');
+            time.textContent = worldApiData.datetime.slice(11,16).toString()
+            
+            let timeZoneAbbreviation = document.querySelector('#time-zone-abbrev');
+            timeZoneAbbreviation.textContent = worldApiData.abbreviation;
+            
+            
+            let greeting = document.querySelector('.greeting p');
+            let hourOfDay = Number(worldApiData.datetime.slice(11,13))
+
+            if (hourOfDay >= 5 && hourOfDay <= 12){
+                greeting.textContent = 'GOOD MORNING'
+            } else if (hourOfDay >= 12 && hourOfDay <= 18){
+                greeting.textContent = "GOOD AFTERNOON"
+            } else {
+                greeting.textContent = "GOOD EVENING"
+            }
+
+            let timeZone = document.querySelector('.time-zone-full')
+            timeZone.textContent = worldApiData.timezone
+            let dayOfYear = document.querySelector('.day-of-the-year')
+            dayOfYear.textContent = worldApiData.day_of_year
+            let dayOfWeek = document.querySelector('.day-of-the-week')
+            dayOfWeek.textContent = worldApiData.day_of_week
+            let weekNumber = document.querySelector('.week-number')
+            weekNumber.textContent = worldApiData.week_number
+
+    }
+};
+worldApi.open("GET", `https://worldtimeapi.org/api/ip`, true);
+worldApi.send();
+
+
+let d1 = new Date()
+console.log(d1.toString())
--- clock-app/README.md
+++ clock-app/README.md
@@ -14,8 +14,17 @@ Thanks for purchasing this premium Frontend Mentor coding challenge.
 
 Your challenge is to build out this clock application and get it looking as close to the design as possible. You'll be using the following APIs to retrieve the necessary data:
 
+
+
+
+
 - [World Time API](http://worldtimeapi.org/) to set the time based on the visitor's IP adress. This API will also be used for additional data, like the day of the year shown in the expanded state.
 - [IP Geolocation API](https://freegeoip.app/) to set the city and country underneath the time
+
+
+
+
+
 - [Programming Quotes API](https://programming-quotes-api.herokuapp.com/) to generate random programming quotes.
     - If the Programming Quotes API doesn't work, [here's a good alternative quote API](https://github.com/lukePeavey/quotable) you can use instead. It's not programming specific, but it will do the trick.
 
