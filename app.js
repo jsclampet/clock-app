2d4f98d91a2045f8e29fb4959d20ad149ec96b8a
John Clampet <jsclampet@gmail.com>
2022-10-13 18:52:57 -0600

    mobile dev, fixed background image reference and logic for time

--- app.js
+++ app.js
@@ -40,61 +40,62 @@ locationApi.open("GET", "https://api.ipbase.com/v2/info?format=json");
 locationApi.setRequestHeader("apikey", "y9FpuihLeu2pDN80wl9Nfx7vVk8UbfGWklGOMVV5");
 locationApi.send();
 
+setInterval(() => {
+    var worldApi = new XMLHttpRequest();
+            worldApi.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
 
-var worldApi = new XMLHttpRequest();
-        worldApi.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
+                let worldApiData = JSON.parse(worldApi.responseText)
+                console.log('worldApi', worldApiData)
+                
+                let time = document.getElementById('time');
+                time.textContent = worldApiData.datetime.slice(11,16).toString()
+                
+                let timeZoneAbbreviation = document.querySelector('#time-zone-abbrev');
+                timeZoneAbbreviation.textContent = worldApiData.abbreviation;
+                
+                
+                let greeting = document.querySelector('.greeting p');
+                let hourOfDay = Number(worldApiData.datetime.slice(11,13))
 
-            let worldApiData = JSON.parse(worldApi.responseText)
-            console.log('worldApi', worldApiData)
-            
-            let time = document.getElementById('time');
-            time.textContent = worldApiData.datetime.slice(11,16).toString()
-            
-            let timeZoneAbbreviation = document.querySelector('#time-zone-abbrev');
-            timeZoneAbbreviation.textContent = worldApiData.abbreviation;
-            
-            
-            let greeting = document.querySelector('.greeting p');
-            let hourOfDay = Number(worldApiData.datetime.slice(11,13))
+                if (hourOfDay >= 5 && hourOfDay <= 12){
+                    greeting.textContent = 'GOOD MORNING'
+                } else if (hourOfDay >= 12 && hourOfDay <= 18){
+                    greeting.textContent = "GOOD AFTERNOON"
+                } else {
+                    greeting.textContent = "GOOD EVENING"
+                }
 
-            if (hourOfDay >= 5 && hourOfDay <= 12){
-                greeting.textContent = 'GOOD MORNING'
-            } else if (hourOfDay >= 12 && hourOfDay <= 18){
-                greeting.textContent = "GOOD AFTERNOON"
-            } else {
-                greeting.textContent = "GOOD EVENING"
-            }
+                let expandedContainer = document.querySelector('.expanded-container');
+                let icon = document.querySelector('.icon');
+                console.log('hour = ', hourOfDay)
+                if (hourOfDay >= 5 && hourOfDay < 18){
+                    icon.src = 'assets/desktop/icon-sun.svg';
+                    document.body.style.backgroundImage = 'url(/assets/mobile/bg-image-daytime.jpg)';
+                    expandedContainer.style.backgroundColor = '#D2D5D8';
+                    expandedContainer.style.color = '#303030';
+                } else {
+                    icon.src = 'assets/desktop/icon-moon.svg';
+                    document.body.style.backgroundImage = 'url(/assets/mobile/bg-image-nighttime.jpg)';
+                    expandedContainer.style.backgroundColor = '#000000BF';
+                    expandedContainer.style.color = 'white';
+                }
+                
 
-            let expandedContainer = document.querySelector('.expanded-container');
-            let icon = document.querySelector('.icon');
-            if (hourOfDay >= 5 && hourOfDay <= 18){
-                icon.src = 'assets/desktop/icon-moon.svg';
-                document.body.style.backgroundImage = 'url(/assets/mobile/bg-image-daytime.jpg)';
-                expandedContainer.style.backgroundColor = '#D2D5D8';
-                expandedContainer.style.color = '#303030';
-            } else {
-                icon.src = 'assets/desktop/icon-moon.svg';
-                document.body.style.backgroundImage = 'url(/assets/mobile/bg-image-nighttime.jpg)';
-                expandedContainer.style.backgroundColor = 'rgba(0, 0, 0)';
-                expandedContainer.style.color = 'white';
-            }
-            
+                let timeZone = document.querySelector('.time-zone-full')
+                timeZone.textContent = worldApiData.timezone
+                let dayOfYear = document.querySelector('.day-of-the-year')
+                dayOfYear.textContent = worldApiData.day_of_year
+                let dayOfWeek = document.querySelector('.day-of-the-week')
+                dayOfWeek.textContent = worldApiData.day_of_week
+                let weekNumber = document.querySelector('.week-number')
+                weekNumber.textContent = worldApiData.week_number
 
-            let timeZone = document.querySelector('.time-zone-full')
-            timeZone.textContent = worldApiData.timezone
-            let dayOfYear = document.querySelector('.day-of-the-year')
-            dayOfYear.textContent = worldApiData.day_of_year
-            let dayOfWeek = document.querySelector('.day-of-the-week')
-            dayOfWeek.textContent = worldApiData.day_of_week
-            let weekNumber = document.querySelector('.week-number')
-            weekNumber.textContent = worldApiData.week_number
-
-    }
-};
-worldApi.open("GET", `https://worldtimeapi.org/api/ip`, true);
-worldApi.send();
-
+        }
+    };
+    worldApi.open("GET", `https://worldtimeapi.org/api/ip`, true);
+    worldApi.send();
+}, 200);
 
 
 //EXPAND BUTTON
@@ -124,3 +125,21 @@ button.addEventListener('click', () => {
     }   
 
 })
+
+// /// CORRECTED GREETING ICON BASED ON TIME
+// /// 
+
+
+//TABLET MODE NOTES 
+
+    //Greeting now has ... "GOOD MORNING, IT'S CURRENTLY"
+        //maybe use media size in js to textContent += ', IT'S CURRENTLY'
+        //if not, can just add hidden text next to it, or in place of
+
+    //Expanded still uses flex, but now spreads out to rows of 2 cols
+        //Expanded Div flex-direction: row
+        //Expanded info fields are now flex-direction: column
+
+    //CHECK FONT SIZES IN CSS
+
+console.log('date     ', new Date())
